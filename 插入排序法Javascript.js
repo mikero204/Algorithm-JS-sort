615e24ca8d783a266e297bad9e0a3a992a1c10aa
Mike <s310143@hotmail.com>
2017-05-12 18:09:31 +0800

    'update'

--- 插入排序法Javascript.js
+++ 插入排序法Javascript.js
@@ -1,6 +1,21 @@
 //Insertion Sort
-var arr = [43,6,13,40,20,90]
-           
+var arr = [43,6,13,1,80,90]
+
+function insert(arr){
+    for(var i = 1 ; i < arr.length ; i++){
+        var tmp = arr[i]
+        for(var j = i ; j > 0 && tmp < arr[j-1] ; j--){
+            arr[j] = arr[j-1]
+        }
+        arr[j] = tmp 
+    }
+    console.log(arr)
+}
+
+insert(arr)
+
+
+
 var insertionSort = function(data){
     var i, j, tmp;
     for(i = 1; i < data.length; i++){
@@ -13,3 +28,5 @@ var insertionSort = function(data){
     console.log(data)
 };
 insertionSort(arr);
+
+
